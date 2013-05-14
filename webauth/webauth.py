3298723f8cefb035e13f5f453ac3b2371d170f5d
zenglizhi <zenglizhi@jiuqi.com.cn>
2013-05-14 18:02:54 +0800

    remove port:   illegal port number

--- webauth/webauth.py
+++ webauth/webauth.py
@@ -3,7 +3,7 @@ username = "zeng***" # Domain user name
 password = "***"     # Domain user password
 headers = {'Content-Type':'application/x-www-form-urlencoded','Accept':'text/html,application/xml'}
 params=urllib.urlencode({'username':username,'password':password,'pwd':password,'secret':'true'})
-conn = httplib.HTTPConnection("192.168.255.252:80")
+conn = httplib.HTTPConnection("192.168.255.252")
 conn.request("POST","/webAuth/",params,headers)
 response = conn.getresponse()
 print response.status,response.reason # http status code,200 OK 
